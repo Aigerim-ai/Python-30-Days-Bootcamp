792ac851082c3fcd6b9a645fc6af04dfa1cd3415
Aigerim Suleimenova <aigerim.suleimenova27@gmail.com>
2025-06-18 19:09:11 +0500

    solution1
    Signed-off-by: Aigerim Suleimenova <aigerim.suleimenova27@gmail.com>

---
+++ 2.Variables/solution.py
@@ -0,0 +1,11 @@
+name = "Aigerim"
+age = 27
+height = 1.7
+is_student = False
+
+print(f"My name is  {name} I am  {age} years old. My height is {height} meters.Am I a student? {is_student}")
+
+age += 1
+is_student = True
+
+print(f"My name is  {name} I am  {age} years old. My height is {height} meters.Am I a student? {is_student}")
