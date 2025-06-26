e3fafcac85c5c4fdf6b4b455cee456a25e8053b2
Aigerim Suleimenova <aigerim.suleimenova27@gmail.com>
2025-06-26 22:24:25 +0500

    exercise 4
    Signed-off-by: Aigerim Suleimenova <aigerim.suleimenova27@gmail.com>

---
+++ 4.Expression & String/solution.py
@@ -0,0 +1,28 @@
+#[ (8 + 2) \times 3 - 4 / 2
+result = (8 + 2)*3 - 4/2
+print(result)
+#2
+result2 = 5 + 3 * 3 ** 2
+print(result2)
+#3
+x = "Python"
+y = "Programming"
+print(x +" "+ y)
+# Write a program that takes a string input from the user and prints it three times using the * operator.
+str_input = input()
+print(str_input*3)
+#"Python is fun!"
+#Let's learn Python.
+#Path: C:\Users\Python
+
+print("Python is fun!\nLet's learn Python.\nPath: C:\\Users\\Python")
+#Python supports:
+#- Strings
+#- Numbers
+#- Boolean values
+#- Data Structures
+print("""Python supports: 
+- Strings 
+- Numbers 
+- Boolean values  
+- Data Structures""")
