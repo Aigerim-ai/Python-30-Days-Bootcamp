dd209d51da26f4fd6228d8488e60b7486f76dac3
Aigerim Suleimenova <aigerim.suleimenova27@gmail.com>
2025-06-27 13:16:16 +0500

    exercise 5|
    Signed-off-by: Aigerim Suleimenova <aigerim.suleimenova27@gmail.com>

---
+++ 5.Operators/solution.py
@@ -0,0 +1,39 @@
+#4
+#Logical Operators:
+#Given p = True and q = False, print the result of:
+#p and q
+#p or q
+#not p
+p = True
+q = False
+print(p and q)
+print(p or q)
+print(not p)
+#5
+list1 = [1, 2, 3]
+list2 = [1, 2, 3]
+list3 = list1
+print(list3 is list1)
+print(list2 is list1)
+#6
+text = "Hello, Python!"
+print("Java" in text)
+#7
+#Bitwise Operators:
+#Given a = 5 (101 in binary) and b = 3 (011 in binary), compute and print:
+#a & b
+#a | b
+#a ^ b
+#~a
+#a << 1
+#b >> 1
+a = 5
+b = 3
+print(a & b)
+print(a | b)
+print(a << 1)
+print(b >> 1)
+print(~a)
+print(a ^ b)
+
+
